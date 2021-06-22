379af6a094d7fa964a9805bb78fe02fd9da99d4f
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-06-21 21:51:41 -0400

    Add files via upload

---
+++ Most Common Shootahs.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from numpy import genfromtxt
+import os
+import urllib
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import matplotlib.colors as colors
+# from colors import rgb, hex
+import matplotlib.cm as cm
+import struct
+import sys
+
+pd.options.mode.chained_assignment = None
+data = pd.read_csv('2021_reg_pbp.csv')
+data = data.loc[data['EVENTMSGTYPE'] == 1]
+names = pd.read_csv("stats_nba_player_data_2020-21.csv")
+
+shots = pd.DataFrame(0, index=range(150), columns=range(1))
+# name = "Zion Williamson"
+for x in range(0, len(names)):
+    name = names['PLAYER_NAME'].iloc[x]
+    data_h = data.loc[data['PLAYER1_NAME'] == name]
+    # data_h = data_h.loc[data_h['EVENTMSGTYPE'] == 1]
+    shots[name] = np.nan
+    news = data_h['EVENTMSGACTIONTYPE'].value_counts()
+    news = news.sort_index()
+    shots[name] = news
+    # shots.join(news)
+
+print(shots)
+del shots[0]
+shots['indexer'] = shots.index.values
+shots.to_csv("shot_types.csv", index=False)
---
+++ Shot Type Analysis.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from numpy import genfromtxt
+import os
+import urllib
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import matplotlib.colors as colors
+# from colors import rgb, hex
+import matplotlib.cm as cm
+import struct
+import sys
+
+SHOTS = {1:'Normal Jumper', 2:'Running Jumper', 3:'Hook Shot', 5:'Normal Layup',
+        6:'Driving Layup', 7:'Normal Dunk', 9:'Driving Dunk', 41:'Running Layup', 43:'Alley-Oop Layup',
+        44:'Reverse Layup', 47:'Turnaround Jumper', 50:'Running Dunk', 51:'Reverse Dunk',
+        52:'Alley-Oop Dunk', 57:'Driving Hook Shot', 58:'Turnaround Hook Shot', 63:'Fadeaway Jumper',
+        66:'Jump Shot (Bank)', 67:'Hook Shot (Bank)', 71:'Finger Roll Layup', 72:'Putback Layup',
+        73:'Driving Reverse Layup', 74:'Running Reverse Layup', 75:'Driving Finger Roll Layup',
+        76:'Running Finger Roll Layup', 78:'Floater', 79:'Pullup Jumper', 80:'Stepback Jumper',
+        86:'Turnaround Fadeaway Jumper', 87:'Putback Dunk', 93:'Driving Hook Shot(Bank)',
+        96:'Turnaround Hook Shot (Bank)', 97:'Tip-in Layup', 98:'Cutting Layup',
+        99:'Cutting Finger Roll Layup', 100:'Running Alley-Oop Layup', 101:'Driving Floater',
+        102:'Driving Floater (Bank)', 103:'Running Pullup Jumper', 104:'Stepback Jumper (Bank)',
+        105:'Turnaround Fadeaway Jumper (Bank)', 106:'Running Alley-Oop Dunk', 107:'Tip-in Dunk',
+        108:'Cutting Dunk', 109:'Driving Reverse Dunk', 110:'Running Reverse Dunk',
+}
+
+data = pd.read_csv("shot_types.csv")
+
+data = data.drop(columns=['indexer'])
+# data = data.dropna(how='any')
+# for x in range(0, 150):
+df = data.max(axis=1)
+df2 = data.idxmax(axis=1)
+df = df.dropna(how='any')
+df2 = df2.dropna(how='any')
+
+df2 = pd.DataFrame(df2, columns=['Player'])
+# df2.rename(index=SHOTS)
+df2['Shot Types'] = pd.Series(SHOTS)
+df2['Count'] = df
+
+df2.to_csv('shootahs.csv', index=False)
+
